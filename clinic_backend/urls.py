7ebb37ee3a976e3d09689fb1aa2fd538e0567759
Dhivakar024 <dhivakarm205@gmail.com>
2026-07-06 11:47:30 +0530

    Add root endpoint

--- clinic_backend/urls.py
+++ clinic_backend/urls.py
@@ -1,6 +1,14 @@
+from django.http import JsonResponse
 from django.urls import path, include
 
+def home(request):
+    return JsonResponse({
+        "message": "Devs Hair & Clinic Backend is running 🚀"
+    })
+
 urlpatterns = [
+    path("", home),
+
     path("api/auth/", include("accounts.urls")),
     path("api/doctors/", include("doctors.urls")),
     path("api/services/", include("services.urls")),
